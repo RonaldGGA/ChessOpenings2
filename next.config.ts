6e2245f2d287f5dbf8f7bdbe415952afad828b49
unknown <ronald.dearmass@gmail.com>
2025-10-28 22:04:23 -0400

    Add Vercel configuration and update Prisma schema and Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   outputFileTracingIncludes: {
+        '/api/**/*': ['./node_modules/.prisma/client/**/*.node'],
+    },
 };
 
 export default nextConfig;
